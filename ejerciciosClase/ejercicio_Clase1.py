73c3922d86f4f7ba7a2ffc19febe7967c52bc408
MarlonGomez2007 <167369490+MarlonGomez2007@users.noreply.github.com>
2024-04-30 10:59:07 -0500

    30/04/2024

---
+++ ejerciciosClase/ejercicio_Clase1.py
@@ -0,0 +1,6 @@
+a = int(input("Ingrese el Primer Valor: "))
+b = int(input("Ingrese el Segundo Valor: "))
+if b !=0:
+    print(a/b)
+else:
+    print("No se puede dividir")
---
+++ ejerciciosClase/ejercicio_Clase2.py
@@ -0,0 +1,5 @@
+x = int(input("Ingrese el Primer Valor: "))
+if x == 5:
+    print("Es 5")
+else:
+    print("No es 5")
